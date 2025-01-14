3c68af91ede2d59fb3aceacc250cf6071c2f828c
Mokarama <mokaramaaktershanta@gmail.com>
2025-01-14 18:45:19 +0600

    button added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('index.js ile added')
+// console.log('index.js ile added')
+
+const counterElement = document.getElementById('container');
+const plusBtn = document.getElementById('plusBtn');
+const minusBtn = document.getElementById('minusBtn');
+
+let counter = 0;
+
+
+plusBtn.addEventListener('click', () => {
+    // console.log(counter= counter + 1);
+   counter = counter + 1;
+   counterElement.innerText = counter;
+
+});
+
+minusBtn.addEventListener('click', () => {
+    // console.log(counter = counter -1);
+    counter = counter - 1;
+    counterElement.innerText = counter;
+});
